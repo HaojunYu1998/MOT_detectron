8f725624b23ceebdf772aa6301cd5d6d58aa5875
HaojunYuPKU <y1700012804@pku.edu.cn>
2021-10-20 01:26:53 +0800

    update configs

--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
+
+python train_net.py --num-gpus 4 \
+--config-file configs/YOLOX/yolox_nano.yaml \
+OUTPUT_DIR outputs/yolox_nano
+
 python train_net.py --num-gpus 4 \
 --config-file configs/YOLOX/yolox_s.yaml \
-OUTPUT_DIR outputs/yolox_s \
-SOLVER.MAX_ITER 1 \
-MODEL.WEIGHTS ""
+OUTPUT_DIR outputs/yolox_s
+
+python train_net.py --num-gpus 4 \
+--config-file configs/YOLOX/yolox_m.yaml \
+OUTPUT_DIR outputs/yolox_m
+
+python train_net.py --num-gpus 4 \
+--config-file configs/YOLOX/yolox_l.yaml \
+OUTPUT_DIR outputs/yolox_l
+
+python train_net.py --num-gpus 4 \
+--config-file configs/YOLOX/yolox_x.yaml \
+OUTPUT_DIR outputs/yolox_x
