8aef10cfc81dda4503c8b150264c7568e52fbb25
HaojunYuPKU <y1700012804@pku.edu.cn>
2021-10-19 20:52:38 +0800

    fix some bugs

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python train_net.py --num-gpus 4 \
---config-file configs/YOLOX/yolox_nano_10000_iters.yaml \
-OUTPUT_DIR outputs/debug \
-SOLVER.MAX_ITER 1000
+python train_net.py --num-gpus 4 --eval-only \
+--config-file configs/YOLOX/yolox_nano_50000_iters.yaml \
+OUTPUT_DIR outputs/yolox_nano_50000_iters \
+MODEL.WEIGHTS outputs/yolox_nano_50000_iters/model_final.pth
